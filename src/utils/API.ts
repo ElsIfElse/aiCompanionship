a8186efbaea996a833583e33e0ba0f24a4261943
Sapimizera <szabolcs.sapi@gmail.com>
2024-12-23 12:09:03 +0100

    updated URL API

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,2 +1,2 @@
 // export const API_URL:string = 'http://192.168.0.100:5000/api'
-export const API_URL:string = 'https://talktothem.vercel.app/api'
+export const API_URL:string = 'ai-companionship-1vtj54zj7-szabolcs-sapi-s-projects.vercel.app/api'
