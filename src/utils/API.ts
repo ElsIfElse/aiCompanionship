82f5ae53c8ae1d671f7d2c529d069f0088037d10
Sapimizera <szabolcs.sapi@gmail.com>
2024-12-23 13:36:48 +0100

    set url

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,2 +1,2 @@
-export const API_URL:string = 'http://192.168.0.100:5000/api'
-// export const API_URL:string = 'https://talktothem.vercel.app/api'
+// export const API_URL:string = 'http://192.168.0.100:5000/api'
+export const API_URL:string = 'https://talktothem.vercel.app/api'
