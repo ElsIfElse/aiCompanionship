3ad62ac4c8f0723e5dbeb110a192c9e0db2f0f12
Sapimizera <szabolcs.sapi@gmail.com>
2024-12-23 11:39:19 +0100

    updated API_URL

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,1 +1,2 @@
-export const API_URL:string = 'http://192.168.0.100:5000/api'
+// export const API_URL:string = 'http://192.168.0.100:5000/api'
+export const API_URL:string = 'https://ai-companionship.vercel.app/api'
