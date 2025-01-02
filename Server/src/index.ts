a12ea8017d0d460b492ca5e98ce35bbbc1359912
Sapimizera <szabolcs.sapi@gmail.com>
2025-01-02 17:14:12 +0100

    nowiguess03

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -8,23 +8,24 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: ['https://aicompanionship.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: 'https://aicompanionship.vercel.app',
+    methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
+ 
 
 app.use(express.json())
 app.get("/test",(req,res)=>{
     res.send("Server is running")
 })
+
 app.use("/api",aiRoutes)
 
 
-const PORTNUM:string|undefined = process.env.PORT
+const PORTNUM:string|undefined = process.env.PORT 
 const parsedPortNum:number = PORTNUM ? parseInt(PORTNUM) : 5000
 
-app.listen(parsedPortNum,'0.0.0.0',()=>{
+app.listen(parsedPortNum,()=>{
     console.log(`Server is running on port ${PORTNUM?.toString()}`)
 }) 
 
