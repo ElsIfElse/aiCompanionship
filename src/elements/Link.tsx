bac86343b8aff42d89d2090ba8d5715525cab6ca
Sapimizera <szabolcs.sapi@gmail.com>
2025-01-02 10:50:12 +0100

    Front fix 05

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -14,13 +14,13 @@ const Link01:React.FC<LinkProps> = ({destination,name,action}) => {
     
     return ( 
         <motion.div 
-        
+        className='flex justify-center items-center'
         animate={{opacity:[0,1]}}
         transition={{delay:0.35,duration:0.2}}>
             <Link
             to={{pathname:destination}} onClick={()=>action ? action() : noOpFunction()}
-            ></Link>
-            {name}
+            >{name}</Link>
+            
         </motion.div>
      );
 }
